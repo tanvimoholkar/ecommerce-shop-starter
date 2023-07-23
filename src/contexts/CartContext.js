01f4fb613998234cab33c7bd81577c79080af225
tanvimoholkar <tanvi.moholkar@gmail.com>
2023-07-23 23:58:10 +0530

    first

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+//create context
+export const CartContext = createContext()
+
+const CartProvider = ({children}) => {
+  //cart state
+  const [cart, setCart] = useState([]);
+
+  //add to cart
+  const addToCart = (id) => {
+    console.log(`item ${id} added to the cart`)
+  }
+  return <CartContext.Provider value={{addToCart}}>
+    {children}
+    </CartContext.Provider>;
 };
 
-export default CartContext;
+export default CartProvider;
